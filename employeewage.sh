14331b666f37ea75cb80b894f6f8c81388a9e3ca
vamshi4220 <ud75281@gmail.com>
2020-10-30 17:21:25 +0530

    added monthly wage

--- employeewage.sh
+++ employeewage.sh
@@ -51,3 +51,30 @@ case $ch in
     2)echo "Daily Employee Wage Part Time - "$Daily_employee_wage_part;;
     *)echo "wrong choice";;
 esac
+
+#uc5
+#Variables
+Wage_per_hour=20
+Full_time_hour=8
+Part_time_hour=4
+Working_day_per_month=20
+
+Daily_employee_wage_full=$(($Wage_per_hour*$Full_time_hour))
+
+Daily_employee_wage_part=$(($Wage_per_hour*$Part_time_hour))
+
+monthly_employee_wage_full=$(($Daily_employee_wage_full*$Working_day_per_month))
+
+monthly_employee_wage_part=$(($Daily_employee_wage_part*$Working_day_per_month))
+
+
+echo "enter choice :"
+read ch
+case $ch in
+    1)echo "Daily Employee Wage Full Time - "$Daily_employee_wage_full;;
+    2)echo "Daily Employee Wage Part Time - "$Daily_employee_wage_part;;
+    3)echo "monthly_employee_wage_full - "$monthly_employee_wage_full;;
+    4)echo "monthly_employee_wage_part - "$monthly_employee_wage_part;;
+    *)echo "wrong choice";;
+esac
+
