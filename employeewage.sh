09aa736f487554c15b6e318ce6de5de22c186ec9
vamshi4220 <ud75281@gmail.com>
2020-10-30 15:37:08 +0530

    added part-time wage

--- employeewage.sh
+++ employeewage.sh
@@ -16,3 +16,16 @@ Wage_per_hour=20
 Full_day_hour=8
 Daily_employee_wage=$(($Wage_per_hour*$Full_day_hour))
 echo "Daily Employee Wage - "$Daily_employee_wage
+
+#UC3
+
+#Variables
+Wage_per_hour=20
+Full_time_hour=8
+Part_time_hour=4
+
+Daily_employee_wage_full=$(($Wage_per_hour*$Full_time_hour))
+echo "Daily Employee Wage Full Time - "$Daily_employee_wage_full
+
+Daily_employee_wage_part=$(($Wage_per_hour*$Part_time_hour))
+echo "Daily Employee Wage Part Time - "$Daily_employee_wage_part
